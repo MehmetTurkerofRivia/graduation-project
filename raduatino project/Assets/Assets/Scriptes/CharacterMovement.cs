ef7af504f5ba673023efe7f162100b4b75fd7abf
Mehmet Türker <mertmemo69@gmail.com>
2025-11-09 13:25:07 +0300

    s

--- raduatino project/Assets/Assets/Scriptes/CharacterMovement.cs
+++ raduatino project/Assets/Assets/Scriptes/CharacterMovement.cs
@@ -1,51 +1,27 @@
 using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
-    private float walkSpeed = 1.0f;
-
-    [SerializeField] private float mouseSensetive = 2.0f;
-    [SerializeField] private float upDownRange = 80.0f;
+    private float speed = 100.0f;
+    private float rotationSpeed = 90.0f; // degrees per second
 
     private CharacterController characterController;
 
-    private float verticalRotation;
-
-    private Camera mainCamera;
-
-    [SerializeField] private string horizontalMoveInput = "Horizontal";
-    [SerializeField] private string verticalMoveInput = "Vertical";
-
-    [SerializeField] private string MouseXInput = "Mouse X";
-    [SerializeField] private string MouseYInput = "Mouse Y";
-    private void Start()
+    void Start()
     {
-        characterController = GetComponent<CharacterController>();   
-        mainCamera = Camera.main;
+        characterController = GetComponent<CharacterController>();
     }
 
-    private void Update()
+    void Update()
     {
-        HandleMovment();
-        HandleRotation();
-    }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-    void HandleMovment()                        
-    {
-        float verticalSpeed = Input.GetAxis(verticalMoveInput) * walkSpeed;
-        float horizontalSpeed = Input.GetAxis(horizontalMoveInput) * walkSpeed;
+        // Rotate character
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
 
-        Vector3 speed = new Vector3 (horizontalSpeed, 0, verticalSpeed);
-        speed = transform.rotation * speed;
-        characterController.Move(speed);
-    }
+        // Move character
+        Vector3 moveDirection = transform.forward * verticalInput * speed;
 
-    void HandleRotation()
-    {
-        float mouseXRotation = Input.GetAxis(MouseXInput) * mouseSensetive;
-        transform.Rotate(0, mouseXRotation, 0);
-
-        verticalRotation -= Input.GetAxis(MouseYInput) * mouseSensetive;
-        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
-        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        characterController.SimpleMove(moveDirection);
     }
 }
